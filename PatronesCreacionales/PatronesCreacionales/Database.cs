aff1f368ee931c249a4724dbf482c8d1e8884074
Víctor Martínez <victor.martinez@blueoceantech.com.mx>
2020-01-16 16:46:08 -0600

    Se agrega el ejercicio del Patrón Singleton

---
+++ PatronesCreacionales/PatronesCreacionales/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class Database
+    {
+        private Database() { }
+
+        private static Database instance;
+        
+        public static Database getInstance()
+        {
+            if (instance == null)
+                instance = new Database();
+            return instance;
+        }
+
+        public void query(string sql) {
+             ///Realize the query
+        }
+    }
+}
--- PatronesCreacionales/PatronesCreacionales/Program.cs
+++ PatronesCreacionales/PatronesCreacionales/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace PatronesCreacionales
+namespace Singleton
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Database bd = Database.getInstance();
+            Database bd2 = Database.getInstance();
+
+            if (bd == bd2) {
+                Console.WriteLine("SAME INSTANCE");
+                bd.query("SELECT * FROM BaseDatos");
+            }
         }
     }
 }
